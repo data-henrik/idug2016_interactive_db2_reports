3c7d7c0705bec3a6729ab832d1e2b813abbfbe86
Henrik Loeser <data-henrik@users.noreply.github.com>
2016-11-10 11:27:58 +0100

    Create myvariable.py

---
+++ myvariable.py
@@ -0,0 +1,7 @@
+# my userIDs and passwords, so they don't appear in the notebook
+LocalDB2password="mysecret"
+OWMkey="owmKey"
+
+dashName="dashXXXXXX"
+dashPW="password"
+dashHost="yp-dashdb-small-XX-lonXX.services.eu-gb.bluemix.net"
